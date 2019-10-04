95bfcfee6d87afdfd889f47e6047503b9627ed7d
Julian Kronlachner <julian.kronlachner@lab73.at>
2019-10-04 12:17:12 +0200

    Created Filepaths, Changed index Made theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import {createMuiTheme, responsiveFontSizes} from "@material-ui/core";
+import {red} from "@material-ui/core/colors";
+import {ThemeProvider} from "@material-ui/styles";
+
+import DropSite from "./sites/DropSite";
+
+
+let theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#37BC7A'
+        },
+        secondary:{
+            main: '#7B7B7B'
+        },
+        error: red
+    }
+});
+theme = responsiveFontSizes(theme);
+
+ReactDOM.render(<ThemeProvider theme={theme} children={DropSite}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/sites/DropSite.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { makeStyles} from "@material-ui/styles";
+
+const style = makeStyles({
+
+})
+
+
+const DropSite = () => {
+
+
+
+    return <h1>Hello World</h1>;
+}
+
+export default DropSite;
